b7dfaa03d96d5c9eedafdb5bd966e24136238f4c
Asif Ajrof <asifajrof@gmail.com>
2023-08-29 02:29:55 +0600

    multi run demo file

---
+++ stLearn_run/run_20.py
@@ -0,0 +1,32 @@
+import sys
+import subprocess
+
+seeds = [
+    601,
+    143,
+    845,
+    630,
+    459,
+    123,
+    201,
+    221,
+    58,
+    157,
+    414,
+    42,
+    991,
+    676,
+    115,
+    826,
+    399,
+    79,
+    150,
+    711
+]
+
+dataset = sys.argv[1]
+
+for seed in seeds:
+    print(f"seed: {seed}")
+    subprocess.run(
+        f"python stLearn_run.py drafts/config_{dataset}.json {seed}", shell=True)
